bdaf742432580a44c6e4f81cf8488bdafcb6a58d
Abhishek Singh <iabhishekapp@gmail.com>
2024-05-15 13:34:18 -0700

    feat: Add bulk book upload with detailed feedback
    - Implemented file upload and parsing in Streamlit for bulk book addition.
    - Updated backend to handle bulk additions and provide success/failure feedback.

--- backend.py
+++ backend.py
@@ -1,122 +1,99 @@
-from flask import Flask, request, jsonify, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-class Book(db.Model):
-    id = db.Column(db.String(50), primary_key=True) 
-    title = db.Column(db.String(100))
-    author = db.Column(db.String(100))
-    status = db.Column(db.String(20), default='available')
+client = MongoClient('mongodb+srv://iabhishekapp:BNYopb29Qevxi8Ch@backendpro.5qozij5.mongodb.net/?retryWrites=true&w=majority&appName=BackendPro') #Update env variable
+db = client['library_db']
+users_collection = db['users']
+books_collection = db['books']
 
 @app.route('/book/<book_id>', methods=['GET'])
 def get_book(book_id):
-    book = Book.query.filter_by(id=book_id).first()
+    book = books_collection.find_one({"_id": book_id})
     if book:
-        return jsonify({"id": book.id, "title": book.title, "author": book.author, "status": book.status}), 200
+        return jsonify({"id": str(book["_id"]), "title": book["title"], "author": book["author"], "status": book["status"]}), 200
     else:
         return jsonify({'message': 'Book not found'}), 404
 
 @app.route('/register', methods=['POST'])
 def register():
-    if request.content_type == 'application/json':
-        data = request.json
-    else:
-        data = request.form
+    data = request.json
     username = data.get('username')
     password = data.get('password')
-    if User.query.filter_by(username=username).first():
+    if users_collection.find_one({"username": username}):
         return jsonify({'message': 'User already exists'}), 400
-    new_user = User(username=username, password=password)
-    db.session.add(new_user)
-    db.session.commit()
+    new_user = {"username": username, "password": password}
+    users_collection.insert_one(new_user)
     return jsonify({'message': 'User registered successfully'}), 201
-
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
-    user = User.query.filter_by(username=username, password=password).first()
+    user = users_collection.find_one({"username": username, "password": password})
     if user:
         return jsonify({'message': 'Login successful'}), 200
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
-    
-# @app.route('/login', methods=['POST'])
-# def login():
-#     data = request.json
-#     username = data.get('username')
-#     password = data.get('password')
-#     user = User.query.filter_by(username=username, password=password).first()
-#     if user:
-#         # Redirect to the view_books endpoint after successful login
-#         return redirect(url_for('view_books'))
-#     else:
-#         return jsonify({'message': 'Invalid credentials'}), 401
-
-# @app.route('/view_books', methods=['GET'])
-# def view_books():
-#     books_query = Book.query.all()
-#     books = {book.id: {"title": book.title, "author": book.author, "status": book.status} for book in books_query}
-#     return jsonify(books)    
 
 @app.route('/books', methods=['GET', 'POST'])
 def manage_books():
     if request.method == 'GET':
-        books_query = Book.query.all()
-        books = {book.id: {"title": book.title, "author": book.author, "status": book.status} for book in books_query}
-        response = jsonify(books)
+        books = books_collection.find()
+        books_dict = {str(book["_id"]): {"title": book["title"], "author": book["author"], "status": book["status"]} for book in books}
+        response = jsonify(books_dict)
         response.headers['X-Custom-Header'] = 'CustomHeader'
         return response
     elif request.method == 'POST':
         data = request.json
         book_id = data.get('book_id')
         book_info = data.get('book_info')
-        if Book.query.filter_by(id=book_id).first():
+        if books_collection.find_one({"_id": book_id}):
             return jsonify({'message': 'Book already exists'}), 400
-        new_book = Book(id=book_id, title=book_info['title'], author=book_info['author'], status='available')
-        db.session.add(new_book)
-        db.session.commit()
+        new_book = {"_id": book_id, "title": book_info['title'], "author": book_info['author'], "status": 'available'}
+        books_collection.insert_one(new_book)
         return jsonify({'message': 'Book added successfully'}), 201
+    
+@app.route('/books/bulk', methods=['POST'])
+def add_books_bulk():
+    data = request.json
+    books = data.get('books', [])
+    results = {"added": [], "failed": []}
+
+    for book in books:
+        book_id = book['book_id']
+        book_info = book['book_info']
+        if books_collection.find_one({"_id": book_id}):
+            results["failed"].append({"book_id": book_id, "reason": "Book already exists"})
+        else:
+            new_book = {"_id": book_id, "title": book_info['title'], "author": book_info['author'], "status": 'available'}
+            books_collection.insert_one(new_book)
+            results["added"].append({"book_id": book_id, "title": book_info['title']})
+    return jsonify(results), 201
 
 @app.route('/issue', methods=['POST'])
 def issue_book():
     data = request.json
     book_id = data.get('book_id')
-    book = Book.query.filter_by(id=book_id).first()
-    if not book or book.status == 'issued':
+    book = books_collection.find_one({"_id": book_id})
+    if not book or book['status'] == 'issued':
         return jsonify({'message': 'Book not available'}), 400
-    book.status = 'issued'
-    db.session.commit()
+    books_collection.update_one({"_id": book_id}, {"$set": {"status": 'issued'}})
     return jsonify({'message': 'Book issued successfully'}), 200
 
 @app.route('/return', methods=['POST'])
 def return_book():
     data = request.json
     book_id = data.get('book_id')
-    book = Book.query.filter_by(id=book_id).first()
-    if not book or book.status != 'issued':
+    book = books_collection.find_one({"_id": book_id})
+    if not book or book['status'] != 'issued':
         return jsonify({'message': 'Invalid return'}), 400
-    book.status = 'available'
-    db.session.commit()
+    books_collection.update_one({"_id": book_id}, {"$set": {"status": 'available'}})
     return jsonify({'message': 'Book returned successfully'}), 200
 
-
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all() 
     app.run(debug=True)
